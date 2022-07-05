83acf97f139c6ea911adc251a50cced679fc38e2
romanjbenitez <roman200217@gmail.com>
2022-07-04 21:34:54 -0300

    cree nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styles/header.scss";
+import Logo from "../assets/Logo.png";
+import {AiOutlineHome, AiOutlineAppstoreAdd, AiOutlineBook, AiFillLinkedin ,AiFillMail} from "react-icons/ai";
+import { IoPaperPlaneOutline, IoMailSharp } from "react-icons/io5";
+import { FaGithubAlt } from "react-icons/fa";
+
+
 
 function Header() {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <p>Roman Benitez</p>
+      </div>
+      <div className="nav">
+        <div className="link">
+          <p><AiOutlineHome/></p>
+          <a href="">INICIO</a>
+        </div>
+        <div className="link">
+          <p><AiOutlineBook/></p>
+          <a href="">SOBRE MI</a>
+        </div>
+        <div className="link">
+          <p><AiOutlineAppstoreAdd /></p>
+          <a href="">PROYECTOS</a>
+        </div>
+        <div className="link">
+          <p><IoPaperPlaneOutline /></p>
+          <a href="">CONTACTO</a>
+        </div>
+      </div>
+      <div className="social-icons-container">
+        <FaGithubAlt className="social-icons"/>
+        <AiFillLinkedin className="social-icons"/>
+        <IoMailSharp className="social-icons"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
