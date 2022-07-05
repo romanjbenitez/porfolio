4a6ecfef02699f49dd2e8abb5e53b7b6f1df6ea4
romanjbenitez <roman200217@gmail.com>
2022-07-04 23:08:54 -0300

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/header.scss";
 import Logo from "../assets/Logo.png";
 import {AiOutlineHome, AiOutlineAppstoreAdd, AiOutlineBook, AiFillLinkedin ,AiFillMail} from "react-icons/ai";
@@ -8,6 +8,10 @@ import { FaGithubAlt } from "react-icons/fa";
 
 
 function Header() {
+
+const [active, setActive] = useState(0)
+
+
   return (
     <div className="header">
       <div className="logo">
@@ -15,21 +19,19 @@ function Header() {
         <p>Roman Benitez</p>
       </div>
       <div className="nav">
-        <div className="link">
-          <p><AiOutlineHome/></p>
-          <a href="">INICIO</a>
+        <div className={active ===  0 ?  "link active" : 'link'} onClick={() => {setActive(0)}}>
+          <a href="#"><p><AiOutlineHome/></p> <span>INICIO</span></a>
         </div>
-        <div className="link">
-          <p><AiOutlineBook/></p>
-          <a href="">SOBRE MI</a>
+        <div className={active ===  1 ?  "link active" : 'link'}  onClick={() => {setActive(1)}}>
+          
+          <a href="#"><p><AiOutlineBook/></p><span>SOBRE MI</span></a>
         </div>
-        <div className="link">
-          <p><AiOutlineAppstoreAdd /></p>
-          <a href="">PROYECTOS</a>
+        <div className={active ===  2 ?  "link active" : 'link'}  onClick={() => {setActive(2)}}>
+          
+          <a href="#"><p><AiOutlineAppstoreAdd /></p><span>PROYECTOS</span></a>
         </div>
-        <div className="link">
-          <p><IoPaperPlaneOutline /></p>
-          <a href="">CONTACTO</a>
+        <div className={active ===  3 ?  "link active" : 'link'}  onClick={() => {setActive(3)}}>
+          <a href="#"><p><IoPaperPlaneOutline /></p><span>CONTACTO</span></a>
         </div>
       </div>
       <div className="social-icons-container">
