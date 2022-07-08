9834fdadf4486d717e88e07d64b9a074da486732
romanjbenitez <roman200217@gmail.com>
2022-07-07 22:21:30 -0300

    fix images 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import data from "../data.js";
 import "../styles/projects.scss";
-import Project1 from "../assets/Project1.png";
-import Project2 from "../assets/Project2.png";
-import Project3 from "../assets/Project3.png";
-import Project4 from "../assets/Project4.png";
-import Project5 from "../assets/Project5.png";
-import Project6 from "../assets/Project6.png";
+import Project1 from "../assets/Proyecto1.png";
+import Project2 from "../assets/Proyecto2.png";
+import Project3 from "../assets/Proyecto3.png";
+import Project4 from "../assets/Proyecto4.png";
+import Project5 from "../assets/Proyecto5.png";
+import Project6 from "../assets/Proyecto6.png";
 
 function Projects() {
   return (
